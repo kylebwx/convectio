ab266c570cc2632f50881b2feb4d9065a8f11b39
Kyle Brooks <kylebrookswx@gmail.com>
2025-10-02 08:23:50 -0400

    longify uses straight line approx

--- src/mesopy/spatial_extensions/longify.py
+++ src/mesopy/spatial_extensions/longify.py
@@ -30,13 +30,19 @@ def get_distance(
         return pd.Series([], dtype=float)
 
         # Check if a starting point was provided
-    if reference_latitude is None or reference_longitude is None:
+    if reference_latitude is None and reference_longitude is None:
         warnings.warn(
-            "No reference coordinates provided! Using the first data point as the origin.",
+            "No reference coordinates provided! Using the closest data point as the origin.",
             UserWarning,
         )
         reference_latitude = transect_df["lat"].iloc[0]
         reference_longitude = transect_df["lon"].iloc[0]
+
+    if reference_latitude is None and reference_longitude is not None:
+        print(
+            "NOTICE: No latitude given; assuming straight-line longitudinal approximation."
+        )
+        reference_latitude = transect_df["lat"].iloc[0]
 
     distances = []
 
@@ -48,6 +54,6 @@ def get_distance(
         # Geodesic distance handles the curvature of the earth
         segment_distance = geodesic(current_point, next_point).m
         distances.append(segment_distance)
-        current_point = next_point
+        current_point[0] = next_point[0]
 
     return pd.Series(distances)
